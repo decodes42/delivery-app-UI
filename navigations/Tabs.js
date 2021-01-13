398c6ed3bf7b6bb67b8351e865b9018708a66fc6
Dennis <driverajr42@gmail>
2021-01-13 16:16:44 -0500

    designed bottom tab buttons to float when selected. Returned to normal when not selected.

--- navigations/Tabs.js
+++ navigations/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Image, TouchableOpacity } from 'react-native'
+import Svg, {Path} from 'react-native-svg'
 
 import { createBottomTabNavigator, BottomTabBar } from "@react-navigation/bottom-tabs";
 
@@ -8,6 +9,59 @@ import { COLORS, icons } from '../constants/'
 
 
 const Tab = createBottomTabNavigator();
+
+const TopBarCustomButton = ({accessibilityState, children, onPress}) => {
+    let isSelected = accessibilityState.selected
+    if(isSelected) {
+        return (
+            <View style={{ flex: 1, alignItems: 'center'}}>
+                <View style={{ flexDirection: 'row', position: 'absolute', top: 0}}>
+                    <View syle={{ flex: 1, backgroundColor: COLORS.white }}></View>
+                    <Svg
+                        width={75}
+                        height={61}
+                        viewBox='0 0 75 61'
+                    >
+                        <Path
+                             d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+                             fill={COLORS.white}
+                        />
+                    </Svg>
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+                </View>
+                <TouchableOpacity
+                    style={{
+                        top: -22.5,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: 50,
+                        height: 50,
+                        borderRadius: 25,
+                        backgroundColor: COLORS.white
+                    }}
+                    onPress={onPress}
+                >
+                    {children}
+
+                </TouchableOpacity>
+            </View>
+        )
+    } else {
+        return (
+            <TouchableOpacity
+                style={{
+                    flex: 1,
+                    height: 60,
+                    backgroundColor: COLORS.white
+                }}
+                activeOpacity={1}
+                onPress={onPress}
+            >
+                {children}
+            </TouchableOpacity>
+        )
+    }
+}
 const Tabs = () => {
     return (
         <Tab.Navigator
@@ -36,6 +90,11 @@ const Tabs = () => {
                             }}
                         />
                     ),
+                    tabBarButton: (props) => (
+                        <TopBarCustomButton
+                            {...props}
+                        />
+                    )
                 }}
             />
 
@@ -54,6 +113,11 @@ const Tabs = () => {
                             }}
                         />
                     ),
+                    tabBarButton: (props) => (
+                        <TopBarCustomButton
+                            {...props}
+                        />
+                    )
                 }}
             />
 
@@ -72,6 +136,11 @@ const Tabs = () => {
                             }}
                         />
                     ),
+                    tabBarButton: (props) => (
+                        <TopBarCustomButton
+                            {...props}
+                        />
+                    )
                 }}
             />
 
@@ -90,6 +159,11 @@ const Tabs = () => {
                             }}
                         />
                     ),
+                    tabBarButton: (props) => (
+                        <TopBarCustomButton
+                            {...props}
+                        />
+                    )
                 }}
             />
         </Tab.Navigator>
